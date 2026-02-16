4dfd1b68ec776f0de28109ee04eaddbf8c677572
kiranP <keyrun602@gmail.com>
2026-02-16 12:11:32 +0545

    django deploy

---
+++ session/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render,redirect
+from . models import Student
+from django.contrib.auth.hashers import check_password, make_password
+
+def student_login(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        print(username)
+        try:
+            student=Student.objects.get(username=username)
+            if check_password(password,student.password):
+                request.session['student_id']=student.id
+                request.session['student_name']=student.name
+                return redirect('dashboard')
+            else:
+                return render(request,'session/login.html',{
+                "error":" password invalid"
+            })
+        except:
+            return render(request,'session/login.html',{
+                "error":" user not found"
+            })
+    return render(request,'session/login.html')
+
+def student_dashboard(request):
+    if 'student_id' not in request.session:
+        return redirect('login')
+    
+    name=request.session.get('student_name')
+    return render(request,'session/dashboard.html',{"name":name})
+
+
+def student_logout(request):
+    request.session.flush()
+    return redirect('login')
+
